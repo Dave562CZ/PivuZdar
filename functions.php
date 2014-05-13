2aba23396d554bac9fd78eb8dc2a89360de93f42
spamdr <spamdr@users.noreply.github.com>
2014-05-13 11:14:49 +0200

    PivuZdar 3

--- functions.php
+++ functions.php
@@ -30,4 +30,31 @@ function pivuzdar_theme_setup() {
 	
 }
 
+add_action('init', 'registrace_hospod');
+function registrace_hospod() {
+
+	$labels = array(
+			'name' => __('Hospody', 'pivuzdar'),
+			'singular_name' => __('Hosopoda', 'pivuzdar'),
+			'add_new' => __('Přidat hospodu', 'pivuzdar'),
+			'add_new_item' => __('Přidat novou hospodu', 'pivuzdar'),
+			'edit_item' => __('Změnit hospodu', 'pivuzdar'),
+			'new_item' => __('Nová hospoda', 'pivuzdar'),
+			'view_item' => __('Zobrazit hospodu', 'pivuzdar'),
+	);
+
+	$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => array( 'slug' => 'hospoda', 'with_front' => false),
+			'hierarchical' => true,
+			'supports' => array('title','editor', 'thumbnail'),
+			
+	);
+
+	register_post_type( 'hospody' , $args );
+}
 ?>
